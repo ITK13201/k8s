fcb9287af386d696ec51bdcca6fade8f348540d0
Takumi Ikeda <tkik13201@gmail.com>
2024-03-25 08:39:57 +0900

    modify: nextcloud backup script

--- bin/cronjobs/nextcloud/backup.sh
+++ bin/cronjobs/nextcloud/backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -u
+
 now=$(TZ="Asia/Tokyo" date --iso-8601=seconds)
 
 CURRENT_DIR="/mnt/hdd/data/k8s/pv"
@@ -7,12 +9,15 @@ TARGET_DIR_NAME="nextcloud"
 OUTPUT_DIR="/mnt/hdd-backup/backups/k8s/pv/nextcloud"
 OUTPUT_FILE_NAME="nextcloud-backup-${now}.bz2"
 
-backup () {
-    tar cjvfp "${OUTPUT_DIR}/${OUTPUT_FILE_NAME}" -C ${CURRENT_DIR} ${TARGET_DIR_NAME}
-}
-
-if backup; then
+MESSAGE=$(tar cjvfp "${OUTPUT_DIR}/${OUTPUT_FILE_NAME}" -C ${CURRENT_DIR} ${TARGET_DIR_NAME} |& tee /dev/tty)
+status=$?
+if [[ ${status} = 0 ]]; then
     echo "Backup completed."
+    discord-bot-cli -c "${CHANNEL_ID}" -t "Backup" -d "completed. saved to ${OUTPUT_FILE_NAME}." -l "info"
 else
     echo "Backup failed."
+    CODE_BLOCK_SEPARATOR="\`\`\`"
+    discord-bot-cli -c "${CHANNEL_ID}" -t "Backup" -d "failed.\n${CODE_BLOCK_SEPARATOR}${MESSAGE}${CODE_BLOCK_SEPARATOR}" -l "error"
 fi
+
+exit ${status}
