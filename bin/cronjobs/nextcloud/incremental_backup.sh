04dc0df5d133ef8d36ec607cf00288672b5f0da7
Takumi Ikeda <tkik13201@gmail.com>
2024-03-26 06:16:18 +0900

    modify: backup scripts

--- bin/cronjobs/nextcloud/incremental_backup.sh
+++ bin/cronjobs/nextcloud/incremental_backup.sh
@@ -5,18 +5,19 @@ set -u -o pipefail
 now=$(TZ="Asia/Tokyo" date --iso-8601=seconds)
 
 TARGET_DIR="/mnt/hdd/data/k8s/pv/nextcloud"
-FULL_BACKUP_DIR="/mnt/hdd-backup/backups/k8s/pv/nextcloud/full-backup/nextcloud"
-INCREMENTAL_BACKUP_DIR="/mnt/hdd-backup/backups/k8s/pv/nextcloud/incremental-backup/nextcloud/${now}"
+FULL_BACKUP_BASE_DIR=/mnt/hdd-backup/backups/k8s/pv/nextcloud/full
+LATEST_FULL_BACKUP_DIR=$(find ${FULL_BACKUP_BASE_DIR} -mindepth 1 -maxdepth 1 -type d | sort -n | tail -n 1)
+INCREMENTAL_BACKUP_DIR="/mnt/hdd-backup/backups/k8s/pv/nextcloud/incremental/${now}"
 
 # ===
 
 ### START ###
 discord-bot-cli -c "nextcloud" -t "Incremental backup" -d "Incremental backup started." -l "info"
 
-### FULL BACKUP ###
+### INCREMENTAL BACKUP ###
 discord-bot-cli -c "nextcloud" -t "Incremental backup" -d "Taking a incremental backup..." -l "info"
 mkdir -p "${INCREMENTAL_BACKUP_DIR}"
-message=$(rsync -a --delete --link-dest=${FULL_BACKUP_DIR}/ ${TARGET_DIR}/ "${INCREMENTAL_BACKUP_DIR}"/)
+message=$(rsync -a --delete --link-dest="${LATEST_FULL_BACKUP_DIR}"/ ${TARGET_DIR}/ "${INCREMENTAL_BACKUP_DIR}"/)
 status=$?
 if [[ ${status} = 0 ]]; then
     echo "Incremental backup completed."
