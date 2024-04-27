8ca8e563fbe18592abfe3b1adfd397950c6be8d5
Takumi Ikeda <tkik13201@gmail.com>
2024-04-27 23:33:31 +0900

    modify: added backup scripts of growi

---
+++ bin/cronjobs/growi/full_backup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+APP=growi
+
+set -u -o pipefail
+
+now=$(TZ="Asia/Tokyo" date --iso-8601=seconds)
+
+TARGET_DIR="/mnt/hdd/data/k8s/pv/${APP}"
+FULL_BACKUP_DIR="/mnt/hdd-backup/backups/k8s/pv/${APP}/full/${now}"
+
+# ===
+
+### Create tmpfile ###
+tmpfile=$(mktemp)
+function rm_tmpfile {
+  [[ -f "$tmpfile" ]] && rm -f "$tmpfile"
+}
+trap rm_tmpfile EXIT
+trap 'trap - EXIT; rm_tmpfile; exit -1' INT PIPE TERM
+
+# ===
+
+### START ###
+discord-bot-cli -c "${APP}" -t "Full backup" -d "Full backup started." -l "info"
+
+### FULL BACKUP ###
+discord-bot-cli -c "${APP}" -t "Full backup" -d "Taking a full backup..." -l "info"
+mkdir -p "${FULL_BACKUP_DIR}"
+rsync -a --delete ${TARGET_DIR}/ "${FULL_BACKUP_DIR}"/ 2>&1 | tee "${tmpfile}"
+message=$(cat "${tmpfile}")
+status=$?
+if [[ ${status} = 0 ]]; then
+    echo "Full backup completed."
+    discord-bot-cli -c "${APP}" -t "Full backup" -d "Full backup completed. Saved to ${FULL_BACKUP_DIR}." -l "success"
+else
+    echo "Full backup failed."
+    CODE_BLOCK_SEPARATOR="\`\`\`"
+    discord-bot-cli -c "${APP}" -t "Backup" -d "Failed.${CODE_BLOCK_SEPARATOR}${message}${CODE_BLOCK_SEPARATOR}" -l "error"
+    exit 1
+fi
+
+exit 0
---
+++ bin/cronjobs/growi/incremental_backup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+APP=growi
+
+set -u -o pipefail
+
+now=$(TZ="Asia/Tokyo" date --iso-8601=seconds)
+
+TARGET_DIR="/mnt/hdd/data/k8s/pv/${APP}"
+FULL_BACKUP_BASE_DIR=/mnt/hdd-backup/backups/k8s/pv/${APP}/full
+LATEST_FULL_BACKUP_DIR=$(find ${FULL_BACKUP_BASE_DIR} -mindepth 1 -maxdepth 1 -type d | sort -n | tail -n 1)
+INCREMENTAL_BACKUP_DIR="/mnt/hdd-backup/backups/k8s/pv/${APP}/incremental/${now}"
+
+# ===
+
+### Create tmpfile ###
+tmpfile=$(mktemp)
+function rm_tmpfile {
+  [[ -f "$tmpfile" ]] && rm -f "$tmpfile"
+}
+trap rm_tmpfile EXIT
+trap 'trap - EXIT; rm_tmpfile; exit -1' INT PIPE TERM
+
+# ===
+
+### START ###
+discord-bot-cli -c "${APP}" -t "Incremental backup" -d "Incremental backup started." -l "info"
+
+### INCREMENTAL BACKUP ###
+discord-bot-cli -c "${APP}" -t "Incremental backup" -d "Taking a incremental backup..." -l "info"
+mkdir -p "${INCREMENTAL_BACKUP_DIR}"
+rsync -a --delete --link-dest="${LATEST_FULL_BACKUP_DIR}"/ ${TARGET_DIR}/ "${INCREMENTAL_BACKUP_DIR}"/ 2>&1 | tee "${tmpfile}"
+message=$(cat "${tmpfile}")
+status=$?
+if [[ ${status} = 0 ]]; then
+    echo "Incremental backup completed."
+    discord-bot-cli -c "${APP}" -t "Incremental backup" -d "Incremental backup completed. Saved to ${INCREMENTAL_BACKUP_DIR}." -l "success"
+else
+    echo "Incremental backup failed."
+    CODE_BLOCK_SEPARATOR="\`\`\`"
+    discord-bot-cli -c "${APP}" -t "Incremental backup" -d "Failed.${CODE_BLOCK_SEPARATOR}${message}${CODE_BLOCK_SEPARATOR}" -l "error"
+    exit 1
+fi
+
+exit 0
